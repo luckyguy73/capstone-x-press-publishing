222997d6b84ea2617530b3888612d6ee5481e321
ashton <luckyguy73@yahoo.com>
2018-03-21 22:03:34 -0700

    finished all artists routes

--- server/routers/artists.js
+++ server/routers/artists.js
@@ -6,6 +6,19 @@ const artists = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+artists.param('artistId', (req,res,next,id) => {
+  db.get(`select * from artist where id = ${id};`, (err, row) => {
+    if(err) {
+      next(err);
+    } else if (row) {
+      req.artist = row;
+      next();
+    } else {
+      res.status(404).send();
+    }
+  });
+});
+
 artists.get('/', (req,res,next) => {
   db.all(`select * from artist where is_currently_employed = 1`, (err,rows) => {
     if(err) {
@@ -34,6 +47,44 @@ artists.post('/', (req, res, next) => {
     } else {
       db.get(`SELECT * FROM Artist WHERE id = ${this.lastID}`, (err, row) => {
         res.status(201).send({artist: row});
+      });
+    }
+  });
+});
+
+artists.get('/:artistId', (req,res,next) => {
+  res.status(200).send({artist: req.artist});
+});
+
+artists.put('/:artistId', (req,res,next) => {
+  const newArtist = req.body.artist;
+  if (!newArtist.name || !newArtist.dateOfBirth || !newArtist.biography) {
+    return res.status(400).send();
+  }
+  db.run(`update artist set 
+    name = '${newArtist.name}', 
+    date_of_birth = '${newArtist.dateOfBirth}',
+    biography = '${newArtist.biography}', 
+    is_currently_employed = '${newArtist.isCurrentlyEmployed === 0 ? 0 : 1}'
+    where id = ${req.params.artistId};`, function(err) {
+      if (err) {
+        next(err)   
+      } else {
+        db.get(`SELECT * FROM Artist WHERE id = ${req.params.artistId}`, (err, row) => {
+        res.status(200).send({artist: row});
+      });
+      }
+    });
+});
+
+artists.delete('/:artistId', (req,res,next) => {
+  db.run(`update artist set is_currently_employed = 0 where id = ${req.artist.id};`, 
+    function(err) {
+    if (err) {
+      next(err) 
+    } else {
+      db.get(`select * from artist where id = ${req.artist.id};`, (err, row) => {
+        res.status(200).send({artist: row});
       });
     }
   });
